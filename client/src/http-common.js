3fd924cd6cf032f9563a2c7614ef5180cb3c4a43
Rahul Ladumor <ladumorrahul56@gmail.com>
2020-12-25 22:01:51 +0530

    initial

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://54.172.34.181:8080/api",
   headers: {
     "Content-type": "application/json"
   }
